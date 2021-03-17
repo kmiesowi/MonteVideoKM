563c5ada9d288ea515d6e91fc6bfe2e2b40f6308
kmiesowi <74531408+kmiesowi@users.noreply.github.com>
2021-03-17 13:19:36 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes/routes.js'
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import dotenv from 'dotenv';
+import path from 'path';
+import favicon from 'serve-favicon';
+import cors from 'cors';
+
+dotenv.config();
+const app = express()
+
+const dbURI = process.env.CONNECTION_STRING;
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            swagger: "2.0",
+            title: "MonteVideo API",
+            version: "1.0.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+        },
+        components : {
+            securitySchemes: {
+                Bearer: {
+                    type: "http",
+                    name: "Authorization",
+                    in: "header",
+                    scheme: "bearer"
+                }
+            },
+        }
+    },
+    apis: ["./routes/routes.js"],
+};
+
+console.log(path.resolve());
+app.use(cors());
+app.use(favicon(path.join(path.resolve(), '/public', 'favicon.ico')))
+app.use(express.json());
+app.use('/', routes)
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(async () => {
+        const specs = await swaggerJsdoc(options);
+        app.use(
+            "/api-docs",
+            swaggerUi.serve,
+            swaggerUi.setup(specs)
+        );
+        app.listen(8080, () => {
+            console.log("Server started at localhost: 8080");
+        });
+    })
+    .catch(error => console.error(error));
